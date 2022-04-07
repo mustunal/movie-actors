d372bdab82a734c62e97aa00b27ca222c7ec8377
mustunal <murat.ustunal@gmail.com>
2022-04-07 22:23:44 +0300

    Initial Commit.

---
+++ src/main/java/com/mustunal/movieactors/MovieActorsApplication.java
@@ -0,0 +1,13 @@
+package com.mustunal.movieactors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieActorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieActorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustunal/movieactors/MovieActorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustunal.movieactors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieActorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
