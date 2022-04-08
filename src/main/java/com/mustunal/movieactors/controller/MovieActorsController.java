224e57e3249a4540ba3932e6ea73905cd0271307
mustunal <murat.ustunal@gmail.com>
2022-04-08 18:24:47 +0300

    sayHello api implemented.

--- src/main/java/com/mustunal/movieactors/controller/MovieActorsController.java
+++ src/main/java/com/mustunal/movieactors/controller/MovieActorsController.java
@@ -1,7 +1,16 @@
 package com.mustunal.movieactors.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/movie-actors")
 public class MovieActorsController {
+
+    @GetMapping("/sayHello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
 }
